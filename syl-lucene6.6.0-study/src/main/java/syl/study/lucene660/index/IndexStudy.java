7212af85f3896f0fb53ca236d12cdb63a526123f
史彦磊 <956469025@qq.com>
2017-07-19 21:41:29 +0800

    添加方法

--- syl-lucene6.6.0-study/src/main/java/syl/study/lucene660/index/IndexStudy.java
+++ syl-lucene6.6.0-study/src/main/java/syl/study/lucene660/index/IndexStudy.java
@@ -1,6 +1,12 @@
 package syl.study.lucene660.index;
 
-import org.apache.lucene.document.Document;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.*;
+import org.apache.lucene.index.*;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.store.SimpleFSDirectory;
 
@@ -20,16 +26,43 @@ public class IndexStudy {
 
 
     public static void main(String[] args) throws IOException {
-        FSDirectory dir = new SimpleFSDirectory(Paths.get(INDEX_PATH));
-        Document doc = new Document();
-
+        //创建索引
+//        writeIndex();
+        //查询索引
+        IndexReader reader = DirectoryReader.open(getFsDirectory());
+        IndexSearcher searcher = new IndexSearcher(reader);
+        TermQuery query = new TermQuery(new Term("userName","zhangsan"));
+        TopDocs search = searcher.search(query, 10);
+        ScoreDoc[] scoreDocs = search.scoreDocs;
+        System.out.println(scoreDocs.length);
+        Document doc = searcher.doc(scoreDocs[0].doc);
+        String userName = doc.get("userName");
+        System.out.println(userName);
 
 
     }
 
 
+    private static void writeIndex()throws IOException{
+        FSDirectory dir = getFsDirectory();
+        index(dir);
+    }
 
+    private static void index(FSDirectory dir) throws IOException {
+        Document doc = new Document();
+        doc.add(new StringField("userName","zhangsan", Field.Store.YES));
+        doc.add(new LongPoint("id",1));
+        doc.add(new TextField("content","我是一个小学生,我爱妈妈", Field.Store.YES));
+        IndexWriterConfig conf = new IndexWriterConfig(new StandardAnalyzer());
+        conf.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+        IndexWriter writer = new IndexWriter(dir,conf);
+        writer.addDocument(doc);
+        writer.commit();
+    }
 
+    private static FSDirectory getFsDirectory() throws IOException {
+        return new SimpleFSDirectory(Paths.get(INDEX_PATH));
+    }
 
 
 }
